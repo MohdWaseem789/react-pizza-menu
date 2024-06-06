7d7956571e665a50058c5f58040dc7f3f7f6c45f
Mohd Waseem <waseem4fu2@gmail.com>
2024-06-06 23:02:06 +0530

    some changes in ui

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,54 +1,83 @@
 import React from "react";
 import Pizza from "./Pizza";
 
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
 const Menu = () => {
   return (
     <div className="menu flex flex-col items-center gap-16 py-12">
       <h2 className="w-4/5 text-center text-3xl font-medium uppercase tracking-wider">
         Our Menu
       </h2>
-      <Pizza
-        name="Pizza Prosciutto"
-        ingredients="Tomato, mozzarella, ham, arugula, and burrata cheese"
-        price={18}
-        photoName="/prosciutto.jpg"
-        soldOut={false}
-      />
-      <Pizza
-        name="Focaccia"
-        ingredients="Bread with Italian olive oil and rosemary"
-        price={6}
-        photoName="/focaccia.jpg"
-        soldOut={false}
-      />
-      <Pizza
-        name="Pizza Margherita"
-        ingredients="Tomato and mozzarella"
-        price={10}
-        photoName="/margherita.jpg"
-        soldOut={false}
-      />
-      <Pizza
-        name="Pizza Spinaci"
-        ingredients="Tomato, mozzarella, spinach, and ricotta cheese"
-        price={12}
-        photoName="/spinaci.jpg"
-        soldOut={false}
-      />
-      <Pizza
-        name="Pizza Funghi"
-        ingredients="Tomato, mozzarella, mushrooms, and onion"
-        price={12}
-        photoName="/funghi.jpg"
-        soldOut={true}
-      />
-      <Pizza
-        name="Pizza Salamino"
-        ingredients="Tomato, mozzarella, and pepperoni"
-        price={15}
-        photoName="/salamino.jpg"
-        soldOut={false}
-      />
+      {/* <ul>
+        {pizzaData.map((pizza) => (
+          <li key={pizza.name}>
+            <Pizza
+              name={pizza.name}
+              ingredients={pizza.ingredients}
+              price={pizza.price}
+              photoName={pizza.photoName}
+              soldOut={pizza.soldOut}
+            />
+          </li>
+        ))}
+      </ul> */}
+      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+        {pizzaData.map((pizza) => (
+          <li key={pizza.name}>
+            <Pizza
+              name={pizza.name}
+              ingredients={pizza.ingredients}
+              price={pizza.price}
+              photoName={pizza.photoName}
+              soldOut={pizza.soldOut}
+            />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
